2b920b7bf8964515c033395bf4e4235e204e430e
JannethE-Matute <84471343+JannethE-Matute@users.noreply.github.com>
2022-02-23 10:35:49 -0600

    Update Libro.java

--- remedial/src/main/java/ec/edu/ups/plw/remedial/model/Libro.java
+++ remedial/src/main/java/ec/edu/ups/plw/remedial/model/Libro.java
@@ -1,5 +1,67 @@
 package ec.edu.ups.plw.remedial.model;
 
-public class Libro {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_Libro")
+public class Libro implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	private String editorial;
+	private String titulo;
+	
+	@OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+	@JoinColumn(name = "cat_seccion")
+	private Categoria categoria;
+	
+	@OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+	@JoinColumn(name = "lib_titulo")
+	private List<Autor> autor;
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	public List<Autor> getAutor() {
+		return autor;
+	}
+	public void setAutor(List<Autor> autor) {
+		this.autor = autor;
+	}
+	
+	public String getEditorial() {
+		return editorial;
+	}
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public void addAutores(Autor a) {
+		if(autor ==  null)
+			autor=new ArrayList<Autor>();
+		
+		autor.add(a);
+	}
 }

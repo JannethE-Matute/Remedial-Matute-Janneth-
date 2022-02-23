f5e7838351acc79f61709a4fccc1dc085e005233
JannethE-Matute <84471343+JannethE-Matute@users.noreply.github.com>
2022-02-23 10:35:53 -0600

    Create Reserva.java

---
+++ remedial/src/main/java/ec/edu/ups/plw/remedial/model/Reserva.java
@@ -0,0 +1,21 @@
+package ec.edu.ups.plw.remedial.model;
+
+public class Reserva {
+	
+	private String stock;
+	private Libro libro;
+	
+	public String getStock() {
+		return stock;
+	}
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+	public Libro getLibro() {
+		return libro;
+	}
+	public void setLibro(Libro libro) {
+		this.libro = libro;
+	}
+	
+}

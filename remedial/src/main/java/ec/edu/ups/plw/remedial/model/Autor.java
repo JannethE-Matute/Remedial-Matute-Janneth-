c182fd16979dbd0f1670bd146323c94c4cb99264
JannethE-Matute <84471343+JannethE-Matute@users.noreply.github.com>
2022-02-23 10:35:42 -0600

    Update Autor.java

--- remedial/src/main/java/ec/edu/ups/plw/remedial/model/Autor.java
+++ remedial/src/main/java/ec/edu/ups/plw/remedial/model/Autor.java
@@ -1,5 +1,43 @@
 package ec.edu.ups.plw.remedial.model;
 
-public class Autor {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_Autor")
+public class Autor implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name = "aut_cedula")
+	private String cedula;
+	@Column(name = "aut_nombre")
+	private String nombre;
+	@Column(name = "aut_nacionalidad")
+	private String nacionalidad;
+	
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
 }

901a8ec2bfd84159b0a227e41fb0f6f773384358
JannethE-Matute <84471343+JannethE-Matute@users.noreply.github.com>
2022-02-23 10:35:45 -0600

    Update Categoria.java

--- remedial/src/main/java/ec/edu/ups/plw/remedial/model/Categoria.java
+++ remedial/src/main/java/ec/edu/ups/plw/remedial/model/Categoria.java
@@ -1,5 +1,43 @@
 package ec.edu.ups.plw.remedial.model;
 
-public class Categoria {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_Categoria")
+public class Categoria implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name = "cat_id")
+	private String id;
+	@Column(name = "cat_seccion")
+	private String seccion;
+	@Column(name = "cat_descripcion")
+	private String descripcion;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSeccion() {
+		return seccion;
+	}
+	public void setSeccion(String seccion) {
+		this.seccion = seccion;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 }
